a05c731b9322584fd284b25c7534e533eb0e6ebb
Robert-Best-2116 <Robert.Best2116@gmail.com>
2023-04-13 15:54:00 -0700

    Solo Project

--- Solo Project/client/src/components/incomeAndExpenses/Edit.jsx
+++ Solo Project/client/src/components/incomeAndExpenses/Edit.jsx
@@ -1,8 +1,117 @@
-import React from 'react'
+
+import React, { useEffect, useState} from 'react';
+import axios from 'axios';
+import { useNavigate, useParams, Link } from "react-router-dom";
 
 const Edit = () => {
+
+  //set state, errors and navigate 
+  const [name, setName] = useState("")
+  const [amount, setAmount] = useState(0.00)
+  const [type, setType] = useState("income")
+  const [frequency, setFrequency] = useState("monthly")
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
+
+  //Get Item Information. 
+  useEffect (() => {
+    axios.get(`http://localhost:8000/api/budgetItem/${id}`)
+        .then( res => {
+            console.log(res.data);
+            setName(res.data.budgetItem.name);
+            setAmount(res.data.budgetItem.amount);
+            setType(res.data.budgetItem.type);
+            setFrequency(res.data.budgetItem.frequency);
+        }) 
+        .catch((err) => {
+            console.log(err);
+        });
+  }, []);
+
+  //Edit Submission W/ Navigation to Dashboard
+  const editBudgetItem = (e) => {
+
+    e.preventDefault();
+    
+    axios.patch(`http://localhost:8000/api/budgetItem/${id}`, {
+      name,
+      amount,
+      type,
+      frequency
+    }) 
+      .then(res =>{
+            console.log(res.data);
+            //navigate(`/`);
+    }) 
+      .catch(err => {
+          console.log(err);
+          setErrors(err.response.data.err.errors);
+      })
+    }
+  
+
+
+
+  
   return (
-    <div>update</div>
+    <div class="container">
+      <div class="d-flex justify-content-between mt-3">
+        <h1>Edit an Item</h1>
+        <p><Link to={`/`}>Go Back to Home</Link></p>
+      </div>
+        <div>
+        <form onSubmit={editBudgetItem} class="form-control">
+          <div>
+            <div>
+            <h4>Edit an item</h4>
+            </div>
+            <div class="mb-3">
+              {errors.name ?
+                <p class="text-danger">{errors.name.message}</p> :
+                null
+              } 
+                <label class="form-label">Item Name : </label>
+                <input type="text" value={name} onChange={e => setName(e.target.value)} class="form-control"/>
+            </div>
+            <div class="mb-3">
+              {errors.amount ?
+                <p class="text-danger">{errors.amount.message}</p> :
+                null
+              }
+              <label class="form-label">Amount </label>
+              <input type="number" value={amount} step=".01" onChange={e => setAmount(e.target.value)} class="form-control"/>
+            </div>
+            <div class="mb-3">
+            <label class="form-label">Income or Expense </label>
+              <div>
+                {errors.type ?
+                  <p class="text-danger">{errors.type.message}</p> :
+                  null
+                } 
+                  <select value={type} onChange={(e) => setType(e.target.value) }>
+                      <option value="income">Income</option>
+                      <option value="expense">Expense</option>
+                  </select>
+              </div>
+            <label class="form-label">Frequency </label>
+              <div>
+                {errors.frequency ?
+                  <p class="text-danger">{errors.frequency.message}</p> :
+                  null
+                } 
+                  <select value={frequency} onChange={(e) => setFrequency(e.target.value) }>
+                      <option value="monthly">Monthly</option>
+                      <option value="other">Other</option>
+                  </select>
+              </div>
+            </div>
+            <div>
+              <input type="submit" value="Edit"/> 
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- Solo Project/client/src/components/incomeAndExpenses/View.jsx
+++ Solo Project/client/src/components/incomeAndExpenses/View.jsx
@@ -1,12 +1,45 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+import {Link} from "react-router-dom";
+import { useParams, useNavigate } from 'react-router-dom'
 
 
 //edit button, I would use an onclick, for a button that would then change the state of the text field to a form, that is prepopulated with the information already present from the get request, You can start with a form, they have an attribute called read only true, when declaring your input field in html, when the read only attribute, do {} and it would be the boolean value from useState, either true or false. delete button 
 
 
 const View = () => {
+
+  //Pass in Budget Item ID and set up useState, set up Navigate 
+  const {id} = useParams();
+  const [budgetItem, setBudgetItem] = useState({});
+  const navigate = useNavigate();
+
+  useEffect (() => {
+    axios.get(`http://localhost:8000/api/budgetItem/${id}`)
+        .then( res => {
+            console.log(res.data);
+            setProduct(res.data.product);
+        }) 
+        .catch((err) => {
+            console.log(err);
+        });
+  }, []);
+
+
+
   return (
-    <div>view</div>
+    <div class="container">
+      <div class="d-flex justify-content-between mt-3">
+        <h1>Add an Item</h1>
+        <p><Link to={`/`}>Go Back to Home</Link></p>
+      </div>
+      <div>
+        <p>Name: {budgetItem.name}</p>
+        <p>Amount: {budgetItem.amount}</p>
+        <p>Type: {budgetItem.type}</p>
+        <p>Frequency: {budgetItem.frequency}</p>
+      </div>
+    </div>
   )
 }
 
